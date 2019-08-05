adbc2f9df800ed78f3306f6aa2f67999f134fa3a
lhb0726 <937955281@qq.com>
2019-08-06 00:09:04 +0800

    first part finish

--- src/components/pages/Pos.vue
+++ src/components/pages/Pos.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <el-row>
-            <el-col :span="7">
+            <el-col :span="10" class="pos-order" id="order-list">
                 我是订单
             </el-col>
-            <el-col>
+            <el-col :span="10">
                 我是产品
             </el-col>
         </el-row>
@@ -13,10 +13,17 @@
 
 <script>
 export default {
-    name: 'Pos'
+    name: 'Pos',
+    mounted(){
+        var orderHeight=document.body.clientHeight
+        document.getElementById('order-list').style.height=orderHeight + 'px'
+    }
 }
 </script>
 
 <style  scoped>
-    
+.pos-order{
+    background: #F9FAFC;
+    border-right: 1px solid #C0CCDA; 
+}   
 </style>
