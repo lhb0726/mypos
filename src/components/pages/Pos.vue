c8522c3cb037b04772b0184d01c3008d838726d5
lhb0726 <937955281@qq.com>
2019-08-06 16:47:16 +0800

    picture

--- src/components/pages/Pos.vue
+++ src/components/pages/Pos.vue
@@ -38,14 +38,22 @@
                         </ul>
                     </div>
                 </div>
-                    <div>
-                        <el-tabs class="menu">
-                            <el-tab-pane label="汉堡"></el-tab-pane>
-                            <el-tab-pane label="小食"></el-tab-pane>
-                            <el-tab-pane label="饮料"></el-tab-pane>
-                            <el-tab-pane label="套餐"></el-tab-pane>
-                        </el-tabs>
-                    </div>
+                <div class="goods-type">
+                    <el-tabs class="menu">
+                        <el-tab-pane label="汉堡">
+                            <ul class='cookList'>
+                                <li v-for=" item of type0Goods" :key="item.goodsId">
+                                    <span class="foodImg"><img :src="item.goodsImg" width="100%"></span>
+                                    <span class="foodName">{{item.goodsName}}</span>
+                                    <span class="foodPrice">{{item.price}}</span>
+                                </li>
+                            </ul>
+                        </el-tab-pane>
+                        <el-tab-pane label="小食"></el-tab-pane>
+                        <el-tab-pane label="饮料"></el-tab-pane>
+                        <el-tab-pane label="套餐"></el-tab-pane>
+                    </el-tabs>
+                </div>
             </el-col>
         </el-row>
     </div>
@@ -124,7 +132,61 @@ export default {
               price:17
           }
           
-      ]
+      ],
+      type0Goods:[
+          {
+              goodsId:1,
+              goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+              goodsName:'香辣鸡腿堡',
+              price:18
+          }, {
+              goodsId:2,
+              goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+              goodsName:'田园鸡腿堡',
+              price:15
+          }, {
+              goodsId:3,
+              goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
+              goodsName:'和风汉堡',
+              price:15
+          }, {
+              goodsId:4,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+              goodsName:'快乐全家桶',
+              price:80
+          }, {
+              goodsId:5,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+              goodsName:'脆皮炸鸡腿',
+              price:10
+          }, {
+              goodsId:6,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
+              goodsName:'魔法鸡块',
+              price:20
+          }, {
+              goodsId:7,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+              goodsName:'可乐大杯',
+              price:10
+          }, {
+              goodsId:8,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+              goodsName:'雪顶咖啡',
+              price:18
+          }, {
+              goodsId:9,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+              goodsName:'大块鸡米花',
+              price:15
+          }, {
+              goodsId:20,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+              goodsName:'香脆鸡柳',
+              price:17
+          }
+          
+      ],
         }
     }
 }
@@ -160,9 +222,39 @@ export default {
     margin:5px;
     background-color:#fff;
 } 
-.menu{
-    display: block
+.goods-type{
+    clear: both;
 }
+.cookList li{
+       list-style: none;
+       width:23%;
+       border:1px solid #E5E9F2;
+       height: auot;
+       overflow: hidden;
+       background-color:#fff;
+       padding: 2px;
+       float:left;
+       margin: 2px;
+ 
+   }
+.cookList li span{
+    
+    display: block;
+    float:left;
+}
+.foodImg{
+    width: 40%;
+}
+.foodName{
+    font-size: 18px;
+    padding-left: 10px;
+    color:brown;
 
+}
+.foodPrice{
+    font-size: 16px;
+    padding-left: 10px;
+    padding-top:10px;
+}
 
 </style>
